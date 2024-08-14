217c5e0b25647fb8a1b115b77df50989847a0b08
ggalvisgg <gabrielagalvisgg@gmail.com>
2024-08-14 18:50:12 -0500

    creacion del proyecto

---
+++ src/main/java/com/historia_mascota/HistoriaMascotaApplication.java
@@ -0,0 +1,13 @@
+package com.historia_mascota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaMascotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaMascotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/historia_mascota/HistoriaMascotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historia_mascota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoriaMascotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
